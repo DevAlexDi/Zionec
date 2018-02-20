5dcc4b58d3446061f654f8881e035fa237f4abff
unknown <keitmark@mail.ru>
2018-02-20 12:05:37 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+//     $('.slider-modal-wr').slick({
+//            dots: false,
+//            arrows: true,
+//              infinite: true,
+//              slidesToShow: 1
+//
+//        });
+  
+    
+    
+    
+    
+    
+    
+    
+
+});
